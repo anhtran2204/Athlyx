39e4b08e95686ba3fc571e5ffd2d31f567641d4d
Jason Tran <anhphantran2002@gmail.com>
2026-03-14 21:53:11 -0500

    setup the project with linter, typechecks, pre-commit hooks, and CI workflows, as well as adding basic dependencies and structure for the project directory.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/hints",
+    "@nuxt/fonts",
+  ],
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+    checker: {
+      eslintPath: "eslint",
+    },
+  },
+});
